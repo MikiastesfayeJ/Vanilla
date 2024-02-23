4c1a7fe6a82e1841fa9ec77478f54db754ca3358
MikiasTesfayeJ <Mikiastesfaye998@gmail.com>
2024-02-23 20:03:21 +0300

    cleared textfield when adding items

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -3,11 +3,13 @@
 
 const items = document.querySelector('.lists');
 const item = document.querySelector('.input');
-const add = document.querySelector('.add');
+const add = document.querySelector('.add-button');
 
 add.addEventListener('click', () => {
     const newItem = document.createElement('div');
     newItem.innerHTML = item.value;
     items.append(newItem);
-})
 
+    //to remove the existing text on the textfield
+    item.value = '';
+})
